df1e1ed3cbeb1a4332836c6ef9fd43dadaafc2c7
Виктор Редькин <Moridin830@gmail.com>
2022-02-06 20:14:44 +0300

    Добавьте файлы проекта.

---
+++ Task1/DictionaryOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class DictionaryOperations
+    {
+        public static SortedDictionary<long, string> ToSortedDictionary(Dictionary<string, long> inputDictionary)
+        {
+            SortedDictionary<long, string > result = new SortedDictionary<long, string>();
+            foreach (var key in inputDictionary.Keys)
+            {
+                long value = inputDictionary[key];
+                if (result.ContainsKey(value))
+                {
+                    result[value] += ", " + key;
+                }
+                else
+                {
+                    result.Add(value, key);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task1/TextOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class TextOperations
+    {
+        public static char[] Delimiters { get; } = { ' ', ',', '.', ':', ';', '"', '«', '»', '-', '–', '*', '(', ')', '!', '?', '<', '>', '\t', '\r', '\n' };
+    }
+}
